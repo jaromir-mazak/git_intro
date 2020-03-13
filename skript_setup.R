80ac74ec623ebff90bd9643743734e81a6c707a5
jaromir-mazak <mazak.jaromir@gmail.com>
2020-03-13 12:24:37 +0100

    jen setup

---
+++ skript_setup.R
@@ -0,0 +1,13 @@
+# fill in the missing commands from previous session
+
+usethis::git_vaccinate()
+
+
+install.packages("devtools")
+install.packages("remotes")
+
+remotes::install_github("scholaempirica/reschola") # installing packages from github, not CRAN
+
+devtools::find_rtools()
+
+usethis::use_github()
